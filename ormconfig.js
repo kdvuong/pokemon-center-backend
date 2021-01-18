2d8bc2762d387f12bb0ce20391ef7d0328cdfa52
kdvuong <khangdieuvuong@gmail.com>
2021-01-18 16:28:32 -0600

    chore: prep for deploy

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,11 @@
 module.exports = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'Khang99',
-  database: 'pokemon-center',
-  entities: ['dist/**/*.entity.js'],
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  entities: [process.env.TYPEORM_ENTITIES],
   migrations: ['dist/migrations/*.js'],
   cli: {
     migrationsDir: 'src/migrations',
--- src/app.module.ts
+++ src/app.module.ts
@@ -10,17 +10,17 @@ import { LocalUsersModule } from './users/local/local-users.module';
   imports: [
     LocalUsersModule,
     AuthModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres', // type of our database
-      host: 'localhost', // database host
-      port: 5432, // database host
-      username: 'postgres', // username
-      password: 'Khang99', // user password
-      database: 'pokemon-center', // name of our database,
+      host: process.env.TYPEORM_HOST ?? 'localhost', // database host
+      port: parseInt(process.env.TYPEORM_PORT) ?? 5432, // database host
+      username: process.env.TYPEORM_USERNAME ?? 'postgres', // username
+      password: process.env.TYPEORM_PASSWORD ?? 'Khang99', // user password
+      database: process.env.TYPEORM_DATABASE ?? 'pokemon-center', // name of our database,
       autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
-      synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
+      synchronize: process.env.NODE_ENV === 'production' ? false : true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
     }),
-    ConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
